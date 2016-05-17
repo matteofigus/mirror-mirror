90fd04dc91f5649e2813e7b1ffdeb7c26df12520
Matteo Figus <matteofigus@gmail.com>
2016-05-17 02:00:48 +0100

    fix

--- src/diff.js
+++ src/diff.js
@@ -3,10 +3,11 @@
 var colors = require('colors/safe');
 var fs = require('fs');
 var PNGDiff = require('png-diff');
+var _ = require('lodash');
 
 module.exports = function(before, after, diffFile, callback){
   var image2Stream = fs.createReadStream(after);
   PNGDiff.outputDiff(before, image2Stream, diffFile, false , function(err, diffMetric){
-    callback(err, diffMetric ? diffMetric === 1 : undefined);
+    callback(err, _.isUndefined(diffMetric) ? undefined : diffMetric === 1);
   });
 };
